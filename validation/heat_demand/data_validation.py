253e82aa1fd758d0816abc8ebb1e34724f3a4262
MaGering <marie-claire.gering@rl-institut.de>
2019-11-22 14:44:23 +0100

    Implement suggestions of Pylint check

--- validation/heat_demand/data_validation.py
+++ validation/heat_demand/data_validation.py
@@ -1,15 +1,22 @@
 # -*- coding: utf-8 -*-
-import pandas as pd
-import demandlib.bdew as bdew
+
+"""This module can be used to validate DESTEST heat demand
+The module serves as validation for DESTEST data
+It compares the DESTEST heat demand with data simulated using the demandlib
+"""
+
 import datetime
 import os
+import pandas as pd
+import demandlib.bdew as bdew
 import matplotlib.dates as mdates
-from pandas.plotting import register_matplotlib_converters
-register_matplotlib_converters()
 try:
     import matplotlib.pyplot as plt
 except ImportError:
     plt = None
+
+from pandas.plotting import register_matplotlib_converters
+register_matplotlib_converters()
 
 """
 ----------------------
@@ -19,51 +26,51 @@ except ImportError:
 Processing of DESTEST Heat Demand Data:
     - Calculation of total annual heat demand for each apartment
     - Calculation of apartment with highest and lowest heat demand in DESTEST
-    
 """
 
 # Import DESTEST Data
-path_data_preprocessed = '../data_preprocessed/Destest_heat_demand.csv'     # Data preprocessed in W
-data = pd.read_csv(path_data_preprocessed, delimiter=',', index_col=0)
+# Data preprocessed unit in W
+# imaginary timestamp matches demandlib timestamp and not actual one of data)
+PATH_DATA = '../data_preprocessed/Destest_heat_demand.csv'
+DATA = pd.read_csv(PATH_DATA, delimiter=',', index_col=0)
+IM_TIMESTAMP = '2010-01-01 00:00:00'
+TIME_EXAMPLE = pd.to_datetime(DATA.index, unit='s', origin=IM_TIMESTAMP)
 
-im_timestamp = '2010-01-01 00:00:00'    # imaginary timestamp (Not actual timestamp of data. Same as demandlib)
-time_example = pd.to_datetime(data.index, unit='s', origin=im_timestamp)
 
 # Calculation of value "annual_heat_demand" for bdew.HeatBuilding function in demandlib
 # Create data frame with annual heat demand of each building
-for i in range(1, len(data.columns)+1):
+for i in range(1, len(DATA.columns)+1):
     if i == 1:
-        demand_annual = pd.DataFrame(data={'Apartment No.': [i], 'Total annual demand': [sum(data[str(i)])
-                                                                                         * 900/3600/1000]})\
+        demand_annual = pd.DataFrame(data={'Apartment No.': [i],
+                                           'Total annual demand': [sum(DATA[str(i)])
+                                                                   * 900/3600/1000]})\
             .astype({'Apartment No.': 'int64'})
     else:
-        demand_annual = demand_annual.append({'Apartment No.': i, 'Total annual demand': sum(data[str(i)])
-                                                                                         * 900/3600/1000},
-                                         ignore_index=True).astype({'Apartment No.': 'int64'})
-
+        demand_annual = demand_annual.append({'Apartment No.': i,
+                                              'Total annual demand': sum(DATA[str(i)])
+                                                                     * 900/3600/1000},
+                                             ignore_index=True).astype({'Apartment No.': 'int64'})
 print('\nAnnual heat demand in DESTEST project: \n', demand_annual)
 
 # Save annual demand of apartments in DESTEST project
 demand_annual.to_csv('demand_annual.csv')
 
 # Find the apartment for each minimum and maximum heat demand as representatives
-ap_min_demand = demand_annual['Total annual demand'].idxmin() + 1
-ap_max_demand = demand_annual['Total annual demand'].idxmax() + 1
+AP_MIN_DEMAND = demand_annual['Total annual demand'].idxmin() + 1
+AP_MAX_DEMAND = demand_annual['Total annual demand'].idxmax() + 1
 
 
 """
-
 ----------------------------
     Demandlib simulation
 ----------------------------
 
 Demandlib simulation:
     - Simulation of heat demand of all apartments
-
 """
 # read example temperature series
-datapath = os.path.join(os.path.dirname(__file__), 'example_data.csv')
-temperature = pd.read_csv(datapath)["temperature"]
+DATAPATH = os.path.join(os.path.dirname(__file__), 'example_data.csv')
+TEMPERATURE = pd.read_csv(DATAPATH)["temperature"]
 
 
 # The following dictionary is create by "workalendar"
@@ -91,15 +98,14 @@ def heat_example():
                             periods=8760, freq='H'))
 
     # Heat demand plot for all 24 apartments (efh: Einfamilienhaus)
-    for i in range(0, len(demand_annual)):
+    for j in range(0, len(demand_annual)):
         # Create curve for every apartment in DESTEST regarding its annual heat demand
-        demand['Apartment No. '+str(i+1)] = bdew.HeatBuilding(
-            demand.index, holidays=holidays, temperature=temperature,
-            shlp_type=('EFH'),
-            building_class=1, wind_class=1, annual_heat_demand=demand_annual['Total annual demand'].loc[i],
-            name='Apartment No. '+str(i+1)).get_bdew_profile()
-
-    '''
+        demand['Apartment No. '+str(j+1)] = bdew.HeatBuilding(
+            demand.index, holidays=holidays, temperature=TEMPERATURE,
+            shlp_type='EFH', building_class=1, wind_class=1,
+            annual_heat_demand=demand_annual['Total annual demand'].loc[j],
+            name='Apartment No. '+str(j+1)).get_bdew_profile()
+    """
     ---------------------
         Visualisation
     ---------------------
@@ -107,12 +113,11 @@ def heat_example():
     - Visualisation of heat demand of all apartments
         - DESTEST
         - demandlib simulation
-        
-    - Visualisation of heat demand of the apartments with the highest and lowest annual heat demand
+    - Visualisation of heat demand of the apartments with the highest and lowest
+      annual heat demand
         - DESTEST
         - demandlib simulation
-    
-    '''
+    """
 
     if plt is not None:
 
@@ -121,8 +126,8 @@ def heat_example():
         fig1.suptitle('Heat demand', fontsize=14)
         ax1, ax2 = fig1.subplots(2, 1, sharex=True)
         plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m-%d %H:%M'))
-        for i in range(1, len(data.columns) + 1):
-            ax1.plot(time_example, data[str(i)]/1000, label=str(i))
+        for k in range(1, len(DATA.columns) + 1):
+            ax1.plot(TIME_EXAMPLE, DATA[str(k)]/1000, label=str(k))
         ax1.set_xlabel('Date')
         ax1.set_ylabel('Heat demand in kW')
         ax1.set_title('DESTEST')
@@ -133,19 +138,22 @@ def heat_example():
         ax2.set_title('demandlib')
 
         # Plot heat demand of apartment with maximum and minimum heat demand
-        fig2 = plt.figure('Heat_demand_apartment_' + str(ap_min_demand) + '_and_' + str(ap_max_demand),
-                          figsize=(11.69, 8.27))
-        fig2.suptitle('Heat demand apartment ' + str(ap_min_demand) + ' and ' + str(ap_max_demand), fontsize=14)
+        fig2 = plt.figure('Heat_demand_apartment_' + str(AP_MIN_DEMAND) + '_and_'
+                          + str(AP_MAX_DEMAND), figsize=(11.69, 8.27))
+        fig2.suptitle('Heat demand apartment ' + str(AP_MIN_DEMAND) + ' and '
+                      + str(AP_MAX_DEMAND), fontsize=14)
         ax3, ax4 = fig2.subplots(2, 1, sharex=True)
-        ax3.plot(time_example, data[str(ap_min_demand)]/1000, label=str(ap_min_demand), color='mediumaquamarine')
-        ax3.plot(time_example, data[str(ap_max_demand)]/1000, label=str(ap_max_demand), color='tomato')
+        ax3.plot(TIME_EXAMPLE, DATA[str(AP_MIN_DEMAND)]/1000, label=str(AP_MIN_DEMAND),
+                 color='mediumaquamarine')
+        ax3.plot(TIME_EXAMPLE, DATA[str(AP_MAX_DEMAND)]/1000, label=str(AP_MAX_DEMAND),
+                 color='tomato')
         ax3.set_xlabel('Date')
         ax3.set_ylabel('Heat demand in kW')
         ax3.legend(loc="best")
         ax3.set_title('DESTEST')
 
-        ax4.plot(demand['Apartment No. ' + str(ap_min_demand)], color='mediumaquamarine')
-        ax4.plot(demand['Apartment No. ' + str(ap_max_demand)], color='tomato')
+        ax4.plot(demand['Apartment No. ' + str(AP_MIN_DEMAND)], color='mediumaquamarine')
+        ax4.plot(demand['Apartment No. ' + str(AP_MAX_DEMAND)], color='tomato')
         ax4.set_xlabel('Date')
         ax4.set_ylabel('Heat demand in kW')
         ax4.set_title('demandlib')
